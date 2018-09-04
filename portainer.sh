bdfe9e98e45c4d18c11985c3d3c2c426158a6986
caishaodong <sdcai@fengchaoli.com>
2018-09-04 15:56:47 +0800

    portainer

---
+++ portainer.sh
@@ -0,0 +1,10 @@
+$ docker volume create portainer_data
+$ docker service create \
+--name portainer \
+--publish 9000:9000 \
+--replicas=1 \
+--constraint 'node.role == manager' \
+--mount type=bind,src=//var/run/docker.sock,dst=/var/run/docker.sock \
+--mount type=volume,src=portainer_data,dst=/data \
+portainer/portainer \
+-H unix:///var/run/docker.sock
